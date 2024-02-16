567b39cb8a13770605d41475bb5a7158c49ecc56
Rawatbabita <133135112+Rawatbabita@users.noreply.github.com>
2024-02-16 12:28:18 +0500

    water wasve functionality is added

--- app/src/main/java/com/app/fragments/WaterFragment.java
+++ app/src/main/java/com/app/fragments/WaterFragment.java
@@ -1,65 +1,77 @@
 package com.app.fragments;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.app.flexfusion.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WaterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cjh.WaveProgressBarlibrary.WaveProgressBar;
+
 public class WaterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public WaterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WaterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WaterFragment newInstance(String param1, String param2) {
-        WaterFragment fragment = new WaterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ImageView icAdd;
+    private TextView tvWaterPercentage;
+    private WaveProgressBar waveProgressBar;
+    private int progress = 0;
+    private boolean started = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_water, container, false);
+        View view= inflater.inflate(R.layout.fragment_water, container, false);
+
+        // Initialize views
+        icAdd = view.findViewById(R.id.ic_add);
+        tvWaterPercentage = view.findViewById(R.id.tv_water_percentage);
+        waveProgressBar = view.findViewById(R.id.waveProgressbar);
+
+        waveProgressBar.setTextColor(R.color.black);
+
+        // Create a Timer to update progress
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (started) {
+                    progress++;
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            waveProgressBar.setProgress(progress);
+                            tvWaterPercentage.setText(String.format("0.5L - %d %%", progress));
+                        }
+                    });
+
+                    if (progress == 100) {
+                        progress = 0;
+                    }
+                }
+            }
+        };
+
+        timer.schedule(timerTask, 0, 80);
+
+        // Set onClickListener for the add button
+        icAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                started = !started;
+            }
+        });
+
+        return view;
     }
 }
