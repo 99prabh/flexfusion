9e0ef1bd960429f2f3949f10665bb4998a0a5cfe
99prabh <133125732+99prabh@users.noreply.github.com>
2024-02-18 07:44:12 +0500

    added workout model

---
+++ app/src/main/java/com/app/flexfusion/models/WorkOutModel.java
@@ -0,0 +1,69 @@
+package com.app.flexfusion.models;
+
+import java.io.Serializable;
+
+public class WorkOutModel implements Serializable {
+    String WorkOutName;
+    String Image;
+    String Equipment,TargetMuscle,ExerciseDetails;
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getWorkOutName() {
+        return WorkOutName;
+    }
+
+    public void setWorkOutName(String workOutName) {
+        WorkOutName = workOutName;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getEquipment() {
+        return Equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        Equipment = equipment;
+    }
+
+    public String getTargetMuscle() {
+        return TargetMuscle;
+    }
+
+    public void setTargetMuscle(String targetMuscle) {
+        TargetMuscle = targetMuscle;
+    }
+
+    public String getExerciseDetails() {
+        return ExerciseDetails;
+    }
+
+    public void setExerciseDetails(String exerciseDetails) {
+        ExerciseDetails = exerciseDetails;
+    }
+
+    public WorkOutModel() {
+    }
+
+    public WorkOutModel(String workOutName, String image, String equipment, String targetMuscle, String exerciseDetails) {
+        WorkOutName = workOutName;
+        Equipment = equipment;
+        Image=image;
+        TargetMuscle = targetMuscle;
+        ExerciseDetails = exerciseDetails;
+    }
+}
